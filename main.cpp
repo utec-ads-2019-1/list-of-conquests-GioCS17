07f804bd96ae9baac071b6a351b3a106411709ae
Gio <giordano.alvitez@utec.edu.pe>
2019-03-28 21:19:42 -0500

    Solution UVA 10420

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include<sstream>
+#include<map>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+	
+	string line;
+	string country;
+	int n;
+	cin>>n;
+	cin.ignore();
+	map<string,int> dict;
+	while(n--){
+		getline(cin,line);
+		stringstream s(line);
+		s>>country;
+		if(dict.count(country)==0)
+			dict[country]=1;
+		else
+			dict[country]++;
+
+	}
+
+	for(auto t=dict.begin();t!=dict.end();t++)
+		cout<<t->first<<" "<<t->second<<endl;
     return 0;
 }
